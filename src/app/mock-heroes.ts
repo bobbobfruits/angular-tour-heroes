5905e01f9415e7df23d27f519ee28cfd920e9d92
=bobbobfruits <=>
2018-06-27 17:40:25 -0700

    Chapter5

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'MMMMMMMM'},
+  {id: 12, name: "aiueo"},
+  {id: 13, name: "MMaawfeawefM"},
+  {id: 14, name: "MawfeawfaweM"},
+  {id: 15, name: "bbbbbbbbbbbbb"},
+  {id: 16, name: "UIUX"},
+  {id: 17, name: "Asia University"}
+];
